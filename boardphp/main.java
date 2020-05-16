3db1c3e52459bffcfb5e8d6a46dd8eb7291fc33d
makiron918 <gonzaress918@gmail.com>
2020-05-16 20:19:44 +0900

    Update main.java

--- boardphp/main.java
+++ boardphp/main.java
@@ -1,16 +1,21 @@
 class Main {
   public static void main(String[] args) {
-    // 引数に「Kate Jones」を渡してください
-    printData("Kate Jones", 27);
-    printData("John Christopher Smith", 65);
+    printData(fullName("Kate", "Jones"), 27);
     
+    printData(fullName("John", "Christopher", "Smith"), 65);
   }
 
-  // 引数を受け取るようにしてください
   public static void printData(String name, int age) {
-    // 「私の名前は◯◯です」と出力されるように書き換えてください
     System.out.println("私の名前は" + name + "です");
     System.out.println("年齢は" + age + "歳です");
-    
   }
+
+  public static String fullName(String firstName, String lastName) {
+    return firstName + " " + lastName;
+  }
+  
+  public static String fullName(String firstName, String middleName, String lastName){
+    return firstName + " " + middleName + " " + lastName;
+  }
+  
 }
